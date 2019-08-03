6dd517d89a7ed272b036af5517453fc3d5e61973
mgulham <mgulham@yahoo.com>
2019-08-03 00:32:02 -0700

    Request and response functional. Gifs pushing to index. Add buttons and add css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,12 @@
-var queryURL = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`;
+var villians = ["The Joker", "Bane", "Thanos", "Green Goblin", "Magneto", "Voldemort", "Darth Vader", "Palpatine", "Sauron", "Lex Luthor"]
+var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${villians}&limit=10&offset=0&rating=PG-13&lang=en`;
 
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
       console.log(response);
+      console.log(queryURL)
 
     var gifs = response.data
     for (var i =0; i < gifs.length; i++){
