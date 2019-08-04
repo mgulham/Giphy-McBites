8c5bc5d9da55448b41e5e8cc953dd6ef0887bdce
mgulham <mgulham@yahoo.com>
2019-08-04 01:59:54 -0700

    key test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -49,11 +49,11 @@ renderButtons();
       function displayvillainInfo() {
           $("#villains-view").empty(); //empties gifs from div so new ones can be pushed from api request
         
-        
+          var apiKey = "3N7NX6spXgkUbr96DuzjgxIHZBEnh2fY";
           var villain = $(this).attr("data-name");
 
           //API Key would not link to java on my deploy link for some reason
-          var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=3N7NX6spXgkUbr96DuzjgxIHZBEnh2fY&q="${villain}"&limit=10&offset=0&rating=PG-13&lang=en`
+          var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q="${villain}"&limit=10&offset=0&rating=PG-13&lang=en`
           
         // var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${villain}&limit=10&offset=0&rating=PG-13&lang=en`;
 
